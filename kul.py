f9c0fe91cffcb319c7c0b3f070deccee92c4b049
jingylima <35249698+jingylima@users.noreply.github.com>
2018-06-03 11:58:47 -0700

    Update kul.py

--- kul.py
+++ kul.py
@@ -17,14 +17,14 @@ while "(" in equationa:
 
         if "*" in equationb:
             a = equationb.find("*")
-            c = int(equationb[a - 1]) * int(equation[a + 1])
-            equationb[a-1] = c
+            c = float(equationb[a - 1]) * float(equation[a + 1])
+            equationb[a-1] = str(c)
             equationb[a] = ""
             equationb[a + 1] = ""
         if "/" in equationb:
             a = equationb.find("/")
-            c = equationb[a - 1] / equation[a + 1]
-            equationb[a-1] = c
+            c = float(equationb[a - 1]) / float(equation[a + 1])
+            equationb[a-1] = str(c)
             equationb[a] = ""
             equationb[a + 1] = ""
 
@@ -32,14 +32,14 @@ while "(" in equationa:
 
         if "-" in equationb:
             a = equationb.find("-")
-            c = equationb[a - 1] - equation[a + 1]
-            equationb[a-1] = c
+            c = float(equationb[a - 1]) - float(equation[a + 1])
+            equationb[a-1] = str(c)
             equationb[a] = ""
             equationb[a + 1] = ""
         if "+" in equationb:
             a = equationb.find("+")
-            c = equationb[a - 1] + equation[a + 1]
-            equationb[a-1] = c
+            c = float(equationb[a - 1]) + float(equation[a + 1])
+            equationb[a-1] = str(c)
             equationb[a] = ""
             equationb[a + 1] = ""
     else:
