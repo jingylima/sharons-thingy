ce3b82e8b0075a1f3df1f826363a0d9d7d7c1566
jingylima <35249698+jingylima@users.noreply.github.com>
2018-06-03 12:52:57 -0700

    Update kul.py

--- kul.py
+++ kul.py
@@ -1,51 +1,51 @@
 equationa = "(2 + 2) * 6 + 3"
-equationa.replace (" ", "")
-operators = "+-*/"
+equationa = equationa.replace(' ','')
+#print(equationa)
+operators = "+"
 operators1 = "*/"
-operators2 = "-+"
+operators2 = "+"
 
 while "(" in equationa:
-    start = equationa.find("(")
-    end = equationa.find(")")
-    equationb = (equationa[start:end])
+	start = equationa.find("(")
+	end = equationa.find(")")
+	equationb = (equationa[start:end+1])
+	#print(1)
+	#print(equationb)	
 
  # '''  if equationb[start + 1] in operators:
-    #    for i in range(end)'''
-    if operators in equationb:
-        equationb.replace(" ","")
-    while operators1 in equationb:
+	#	for i in range(end)'''
+	if operators in equationb:
+		equationb.replace(" ","")
+		#print(2)
+	else:
+		break
+	while operators1 in equationb:
 
-        if "*" in equationb:
-            a = equationb.find("*")
-            c = float(equationb[a - 1]) * float(equation[a + 1])
-            equationb[a-1] = str(c)
-            equationb[a] = ""
-            equationb[a + 1] = ""
-        if "/" in equationb:
-            a = equationb.find("/")
-            c = float(equationb[a - 1]) / float(equation[a + 1])
-            equationb[a-1] = str(c)
-            equationb[a] = ""
-            equationb[a + 1] = ""
+		if "*" in equationb:
+			a = equationb.find("*")
+			c = float(equationb[a - 1]) * float(equationb[a + 1])
+			equationb[a-1] = str(c)
+			equationb[a] = ""
+			equationb[a + 1] = ""
+		if "/" in equationb:
+			a = equationb.find("/")
+			c = float(equationb[a - 1]) / float(equationb[a + 1])
+			equationb[a-1] = str(c)
+			equationb[a] = ""
+			equationb[a + 1] = ""
 
-    while operators2 in equationb:
+	while operators2 in equationb:
 
-        if "-" in equationb:
-            a = equationb.find("-")
-            c = float(equationb[a - 1]) - float(equation[a + 1])
-            equationb[a-1] = str(c)
-            equationb[a] = ""
-            equationb[a + 1] = ""
-        if "+" in equationb:
-            a = equationb.find("+")
-            c = float(equationb[a - 1]) + float(equation[a + 1])
-            equationb[a-1] = str(c)
-            equationb[a] = ""
-            equationb[a + 1] = ""
-    else:
-        equationa.replace("equationa[start:end]", equationb)
-        equationb.replace("(","")
-        equationb.replace(")","")
-        equationa.replace(" ","")
-        answer = equationa
-        print(answer)
+		if "-" in equationb:
+			a = equationb.find("-")
+			c = float(equationb[a - 1]) - float(equationb[a + 1])
+			equationb[a-1] = str(c)
+			equationb[a] = ""
+			equationb[a + 1] = ""
+		if "+" in equationb:
+			a = equationb.find("+")
+			c = int(equationb[a - 1]) + int(equationb[a + 1])
+			equationb = equationb.replace(equationb, str(c))
+	equationa = equationa.replace(equationa[start:end+1], equationb)
+	answer = equationa
+print(answer)
